2f322d209ab96f2140061f1ccc4df396183470d5
norman <85535885@qq.com>
2023-01-11 10:15:42 +0800

    use theme colors for colorful loading widget default style.

--- lib/components/colorful_loading.dart
+++ lib/components/colorful_loading.dart
@@ -2,12 +2,6 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 
-const colorizeColors = [
-  Colors.purple,
-  Colors.blue,
-  Colors.yellow,
-  Colors.red,
-];
 
 const colorizeTextStyle = TextStyle(
   fontSize: 50.0,
@@ -16,14 +10,26 @@ const colorizeTextStyle = TextStyle(
 const defaultWaitingTexts = [
   "Loading...","Please", "Wait"
 ];
+// const defaultColors = [
+//   Colors.purple,
+//   Colors.blue,
+//   Colors.yellow,
+//   Colors.red,
+// ];
 class ColorfulLoading extends StatelessWidget {
   final List<String>? texts;
-  const ColorfulLoading({Key? key, this.texts}) : super(key: key);
+  final List<MaterialColor>? colors;
+  const ColorfulLoading({Key? key, this.texts, this.colors}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<String> showingTexts = texts?? defaultWaitingTexts;
-    var textsElems = showingTexts.map((e) => ColorizeAnimatedText(e, textStyle: colorizeTextStyle, colors: colorizeColors)).toList();
+    var theme = Theme.of(context);
+    var defaultColors = [
+      theme.primaryColor,theme.backgroundColor,theme.highlightColor
+    ];
+    var showingTexts = texts?? defaultWaitingTexts;
+    var showingColors = colors?? defaultColors;
+    var textsElems = showingTexts.map((e) => ColorizeAnimatedText(e, textStyle: colorizeTextStyle, colors: showingColors)).toList();
     return
     Center(child:
       AnimatedTextKit(
